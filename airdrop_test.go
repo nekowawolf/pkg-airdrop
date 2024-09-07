f234e0f3c3b5656aa277e9af8a09095554580980
nekowawolf <irukainu20@gmail.com>
2024-09-08 01:00:41 +0700

    update: package

--- airdrop_test.go
+++ airdrop_test.go
@@ -3,12 +3,14 @@ package airdrop
 import (
 	"fmt"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestInsertAirdropFree(t *testing.T) {
-	name := "Beacon"
-	task := "GAME"
-	link := "https://nfq.thebeacon.gg/?referral=0xNekowawolf"
+	name := "Beacon2"
+	task := "GAME2"
+	link := "https://nfq.thebeacon.gg/?referral=0xNekowawolf2"
 	hasil, err := InsertAirdropFree(name, task, link)
 	if err != nil {
 		t.Errorf("Failed to insert AirdropFree: %v", err)
@@ -18,9 +20,9 @@ func TestInsertAirdropFree(t *testing.T) {
 }
 
 func TestInsertAirdropPaid(t *testing.T) {
-	name := "Solv Protocol"
+	name := "Solv Protocol2"
 	task := "HOLD"
-	link := "https://app.solv.finance/points"
+	link := "https://app.solv.finance/points2"
 	hasil, err := InsertAirdropPaid(name, task, link)
 	if err != nil {
 		t.Errorf("Failed to insert AirdropPaid: %v", err)
@@ -48,5 +50,71 @@ func TestGetAllAirdropPaid(t *testing.T) {
 		t.Errorf("No AirdropPaid data found")
 	} else {
 		fmt.Printf("Retrieved AirdropPaid data: %v\n", data)
+	}
+}
+
+func TestGetAirdropFreeByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("66cfa14dd39e7e3b0c85b295") 
+	if err != nil {
+		t.Errorf("Invalid ID format: %v", err)
+		return
+	}
+
+	airdrop, err := GetAirdropFreeByID(id)
+	if err != nil {
+		t.Errorf("Failed to retrieve AirdropFree by ID: %v", err)
+	} else {
+		fmt.Printf("Retrieved AirdropFree by ID: %v\n", airdrop)
+	}
+}
+
+func TestGetAirdropPaidByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("66cfa14dd39e7e3b0c85b297")
+	if err != nil {
+		t.Errorf("Invalid ID format: %v", err)
+		return
+	}
+
+	airdrop, err := GetAirdropPaidByID(id)
+	if err != nil {
+		t.Errorf("Failed to retrieve AirdropPaid by ID: %v", err)
+	} else {
+		fmt.Printf("Retrieved AirdropPaid by ID: %v\n", airdrop)
+	}
+}
+
+func TestUpdateAirdropByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("66dc8cf3b1bc5acfc329268b")
+	if err != nil {
+		t.Fatalf("Invalid ID format: %v", err)
+	}
+
+	err = UpdateAirdropByID("airdrop_paid", id, "git3", "hub3", "https://example.com3")
+	if err != nil {
+		t.Fatalf("Failed to update Airdrop by ID: %v", err)
+	}
+
+	updatedAirdrop, err := GetAirdropFreeByID(id)
+	if err != nil {
+		t.Fatalf("Failed to retrieve updated AirdropFree by ID: %v", err)
+	}
+
+	if updatedAirdrop.Name != "git3" || updatedAirdrop.Task != "hub3" || updatedAirdrop.Link != "https://example.com3" {
+		t.Errorf("AirdropFree data not updated correctly: got %v", updatedAirdrop)
+	}
+}
+
+func TestDeleteAirdropByID(t *testing.T) {
+	id, err := primitive.ObjectIDFromHex("66dc8784c047f376f4c45294")
+	if err != nil {
+		t.Errorf("Invalid ID format: %v", err)
+		return
+	}
+
+	err = DeleteAirdropByID("airdrop_paid", id)
+	if err != nil {
+		t.Errorf("Failed to delete AirdropFree by ID: %v", err)
+	} else {
+		fmt.Printf("Deleted AirdropFree by ID: %v\n", id.Hex())
 	}
 }
--- handlers.go
+++ handlers.go
@@ -2,16 +2,30 @@ package airdrop
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"os"
 )
 
-var MongoString string = os.Getenv("MONGOSTRING")
+var (
+	MongoString string = os.Getenv("MONGOSTRING")
+	database    *mongo.Database
+)
 
+func init() {
+	db, err := MongoConnect("airdrop")
+	if err != nil {
+		fmt.Printf("Failed to connect to MongoDB: %v\n", err)
+		os.Exit(1)
+	}
+	database = db
+}
+
+// MongoConnect initializes a connection to the MongoDB server.
 func MongoConnect(dbname string) (*mongo.Database, error) {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(MongoString))
 	if err != nil {
@@ -24,11 +38,7 @@ func MongoConnect(dbname string) (*mongo.Database, error) {
 	return client.Database(dbname), nil
 }
 
-func InsertOneDoc(db string, collection string, doc interface{}) (interface{}, error) {
-	database, err := MongoConnect(db)
-	if err != nil {
-		return nil, err
-	}
+func InsertOneDoc(collection string, doc interface{}) (interface{}, error) {
 	collectionRef := database.Collection(collection)
 	insertResult, err := collectionRef.InsertOne(context.TODO(), doc)
 	if err != nil {
@@ -39,29 +49,25 @@ func InsertOneDoc(db string, collection string, doc interface{}) (interface{}, e
 
 func InsertAirdropFree(name string, task string, link string) (interface{}, error) {
 	freeAirdrop := AirdropFree{
-		ID:   primitive.NewObjectID(), 
+		ID:   primitive.NewObjectID(),
 		Name: name,
 		Task: task,
 		Link: link,
 	}
-	return InsertOneDoc("airdrop", "airdrop_free", freeAirdrop)
+	return InsertOneDoc("airdrop_free", freeAirdrop)
 }
 
 func InsertAirdropPaid(name string, task string, link string) (interface{}, error) {
 	paidAirdrop := AirdropPaid{
-		ID:   primitive.NewObjectID(), 
+		ID:   primitive.NewObjectID(),
 		Name: name,
 		Task: task,
 		Link: link,
 	}
-	return InsertOneDoc("airdrop", "airdrop_paid", paidAirdrop)
+	return InsertOneDoc("airdrop_paid", paidAirdrop)
 }
 
 func GetAllAirdropFree() ([]AirdropFree, error) {
-	database, err := MongoConnect("airdrop")
-	if err != nil {
-		return nil, err
-	}
 	collection := database.Collection("airdrop_free")
 	cursor, err := collection.Find(context.TODO(), bson.M{})
 	if err != nil {
@@ -75,10 +81,6 @@ func GetAllAirdropFree() ([]AirdropFree, error) {
 }
 
 func GetAllAirdropPaid() ([]AirdropPaid, error) {
-	database, err := MongoConnect("airdrop")
-	if err != nil {
-		return nil, err
-	}
 	collection := database.Collection("airdrop_paid")
 	cursor, err := collection.Find(context.TODO(), bson.M{})
 	if err != nil {
@@ -89,4 +91,86 @@ func GetAllAirdropPaid() ([]AirdropPaid, error) {
 		return nil, fmt.Errorf("GetAllAirdropPaid All: %v", err)
 	}
 	return airdrops, nil
+}
+
+func GetAirdropFreeByID(id primitive.ObjectID) (AirdropFree, error) {
+	collection := database.Collection("airdrop_free")
+	var airdrop AirdropFree
+	err := collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&airdrop)
+	if err != nil {
+		return AirdropFree{}, err
+	}
+	return airdrop, nil
+}
+
+func GetAirdropPaidByID(id primitive.ObjectID) (AirdropPaid, error) {
+	collection := database.Collection("airdrop_paid")
+	var airdrop AirdropPaid
+	err := collection.FindOne(context.TODO(), bson.M{"_id": id}).Decode(&airdrop)
+	if err != nil {
+		return AirdropPaid{}, err
+	}
+	return airdrop, nil
+}
+
+func GetAllAirdrop() ([]interface{}, error) {
+	var allAirdrops []interface{}
+
+	freeAirdrops, err := GetAllAirdropFree()
+	if err != nil {
+		return nil, err
+	}
+	for _, free := range freeAirdrops {
+		allAirdrops = append(allAirdrops, free)
+	}
+
+	paidAirdrops, err := GetAllAirdropPaid()
+	if err != nil {
+		return nil, err
+	}
+	for _, paid := range paidAirdrops {
+		allAirdrops = append(allAirdrops, paid)
+	}
+
+	return allAirdrops, nil
+}
+
+func UpdateAirdropByID(col string, id primitive.ObjectID, name string, task string, link string) error {
+	filter := bson.M{"_id": id}
+
+	update := bson.M{
+		"$set": bson.M{
+			"name": name,
+			"task": task,
+			"link": link,
+		},
+	}
+
+	result, err := database.Collection(col).UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		fmt.Printf("UpdateAirdrop: %v\n", err)
+		return err
+	}
+
+	if result.ModifiedCount == 0 {
+		return errors.New("no data has been changed with the specified ID")
+	}
+
+	return nil
+}
+
+func DeleteAirdropByID(col string, id primitive.ObjectID) error {
+	collection := database.Collection(col)
+	filter := bson.M{"_id": id}
+
+	result, err := collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return fmt.Errorf("error deleting data for ID %s: %s", id.Hex(), err.Error())
+	}
+
+	if result.DeletedCount == 0 {
+		return fmt.Errorf("data with ID %s not found", id.Hex())
+	}
+
+	return nil
 }
