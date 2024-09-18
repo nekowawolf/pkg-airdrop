eea7a65cceb332eac28bb00898ebeee5906ef035
nekowawolf <irukainu20@gmail.com>
2024-09-18 09:14:46 +0700

    update:  struct add level, CreatedAt

--- airdrop.go
+++ airdrop.go
@@ -2,18 +2,23 @@ package airdrop
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type AirdropFree struct {
-    ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-    Name  string             `json:"name,omitempty" bson:"name,omitempty"`
-    Task  string             `json:"task,omitempty" bson:"task,omitempty"`
-    Link  string             `json:"link,omitempty" bson:"link,omitempty"`
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Task      string             `json:"task,omitempty" bson:"task,omitempty"`
+	Link      string             `json:"link,omitempty" bson:"link,omitempty"`
+	Level     string             `json:"level,omitempty" bson:"level,omitempty"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
 
 type AirdropPaid struct {
-    ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-    Name  string             `json:"name,omitempty" bson:"name,omitempty"`
-    Task  string             `json:"task,omitempty" bson:"task,omitempty"`
-    Link  string             `json:"link,omitempty" bson:"link,omitempty"`
+	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string             `json:"name,omitempty" bson:"name,omitempty"`
+	Task      string             `json:"task,omitempty" bson:"task,omitempty"`
+	Link      string             `json:"link,omitempty" bson:"link,omitempty"`
+	Level     string             `json:"level,omitempty" bson:"level,omitempty"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
